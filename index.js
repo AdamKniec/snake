5527f6f301159fcf8ce0610d151d7e62cddbb2e0
AdamKniec <adam.k.kniec@gmail.com>
2019-04-30 20:05:37 +0200

    scoring and bonus food functionality

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ let xSpeed = scale;
 let ySpeed = 0;
 let score = 0;
 let tail = [];
+let scoreBoard = [];
+let colors = ['red', 'yellow', 'white', 'blue', 'brown', 'pink'];
 
 // Snake class, it's design and behaviour
 class Snake {
@@ -31,7 +33,7 @@ class Snake {
         for(let i=0; i<tail.length-1 ; i++) {
             tail[i] = tail[i+1];
         }
-        tail[score-1] = {x:this.x, y:this.y}
+        tail[score-1] = {x:this.x, y:this.y};
 
         this.x += xSpeed;
         this.y += ySpeed;
@@ -69,14 +71,31 @@ class Snake {
         }
     }
     eat(fruit){
-        this.x === fruit.x && this.y === fruit.y ? true : false;
         if(this.x === fruit.x && this.y === fruit.y) {
+            // console.log(context.fillStyle);
             score++;
             return true;
         }
         false;
     }
+    collisionCheck() {
+        for(let i = 0; i< tail.length; i++) {
+            if(this.x === tail[i].x && this.y === tail[i].y) {
+                this.submitScore(score);
+                score = 0;
+                tail = [];
+            }
+        }
+    }
+    submitScore(){
+
+        scoreBoard.push(score);
+            sessionStorage.setItem('Wyniki', scoreBoard);            
+            renderScore();
+    }
 }
+
+
 class Fruit {
     constructor(){
         this.x;
@@ -88,22 +107,60 @@ class Fruit {
     }
     draw(){
         context.fillStyle = 'green';
-        context.fillRect(this.x, this.y, scale,scale)
-    }
+        context.fillRect(this.x, this.y, scale,scale);
+
+        if(score % 5 === 0 && score !== 0) {
+  
+            context.fillStyle = `${colors[Math.floor(Math.random() * colors.length)]}`;
+            context.fillRect(this.x, this.y, scale,scale);
+
+          
+            
+
+            console.log(context.fillStyle);
+            console.log(score)
+
 }
+
+
+    }
+    
+
+}
+
+function renderScore(){
+    let data = sessionStorage.getItem('Wyniki');
+    if(data) {
+        let numericData = data.split(',').map(function(item) {
+            return parseInt(item);
+        }).sort((a,b) => b-a);
+        let scoreContainer = document.querySelector('.score-board'); 
+        let size = 5;
+        const scoreList = ` 
+            <ul class="scoreList">
+                ${numericData.slice(0,size).map((singleScore,i) => `<li>${i+1} Miejsce - ${singleScore} punkty </li>`)}
+            </ul>
+        `;
+    scoreContainer.innerHTML = scoreList;
+   }
+};
 
 (function init() {
     snake = new Snake();
     fruit = new Fruit();
     fruit.getPosition();
-  
+    // bonusFruit = new BonusFruit();
+    renderScore();
     window.setInterval(() => {
         context.clearRect(0,0, canvasBoard.width, canvasBoard.height)
         snake.update();
         snake.draw();
         fruit.draw();
-        snake.eat(fruit) ? fruit.getPosition() : null
-    }, 250)
+        snake.eat(fruit) ? fruit.getPosition() : null;
+        
+        snake.collisionCheck();
+
+    }, 100)
 }())
 
 window.addEventListener('keydown', detectTheKey);
